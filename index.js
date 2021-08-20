c1bdce452b12ba37ad0b4a43ab4696eb960ff130
Arthur Furman <foarthur@gmail.com>
2021-08-20 20:31:42 +0300

    create a simple kenify module

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
-/*
-  TODO: figure out how to strech the input accross page.
-  TODO: add a choser for tiredness level (example options: 'fresh', 'a bit tired', 'very tired', 'ZZZzzz...'). 
-  the more tired the more 'ken' strings will be inserted.
-  TODO: craete a function that gets a sentence and a tiredness level as arguments and 
-  returns a copy of the string with 'ken' in random spaces accross it according to tiredness level.
-  TODO: find an API that generates random sentence and use it.
-  TODO: find an API that for a given sentence returns an audio file of the spoken sentence.
-*/
-
-console.log("hello world");
-
-const tirednessLevels = {
-  0: "fresh as a daisy",
-  1: "a bit tired",
-  2: "just had launch",
-  3: "end of day",
-  4: "ZZZzzz..."
-}
-
-function kenify(sentence, tirednessLevel) {
-  console.log("hello world ken.")
+function kenify(sentence) {
+  const kenifiedSentence = [];
+  sentence.split(" ").forEach((element) => {
+    Math.random() < 0.5
+      ? kenifiedSentence.push(element + " ken")
+      : kenifiedSentence.push(element);
+  });
+  return kenifiedSentence.join(" ");
 }
 
 module.exports = kenify;
